cad01bcf714aa39b0d58f592dc7baeeb39accda9
erehmaryann <edwardmaryann09@gmail.com>
2022-01-12 14:33:25 -0800

    removed image folder, installed contentful

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
-export default function Recipes() {
+import { createClient } from 'contentful';
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  });
+
+  const res = await client.getEntries({
+    content_type: 'recipe',
+  });
+
+  return {
+    props: {
+      recipes: res.items,
+    }
+  };
+}
+
+export default function Recipes({ recipes }) {
+  console.log(recipes);
   return (
     <div className="recipe-list">
       Recipe List
     </div>
-  )
+  );
 }

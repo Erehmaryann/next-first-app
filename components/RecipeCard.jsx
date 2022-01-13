d4ffaf5a9e89588819ecd93e7508cbec95d3ba37
erehmaryann <edwardmaryann09@gmail.com>
2022-01-13 11:24:56 -0800

    fixed image src issue

--- components/RecipeCard.jsx
+++ components/RecipeCard.jsx
@@ -1,10 +1,18 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const RecipeCard = ({ recipe }) => {
 	const { title, slug, cookingTime, thumbnail } = recipe.fields;
 	return (
 		<div className='card'>
-			<div className='featured'>{/* Image - thumb */}</div>
+			<div className='featured'>
+				<Image
+					src={`https:${thumbnail.fields.file.url}`}
+					alt={thumbnail.fields.title}
+					width={thumbnail.fields.file.details.image.width}
+					height={thumbnail.fields.file.details.image.height}
+				/>
+			</div>
 			<div className='content'>
 				<div className='info'>
 					<h4>{title}</h4>

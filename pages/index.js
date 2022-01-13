95c90ca64dbf77420230fa31ddcf519aaa1f6e70
erehmaryann <edwardmaryann09@gmail.com>
2022-01-13 13:39:00 -0800

    styled recipe card comp

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,15 @@ export default function Recipes({ recipes }) {
         <RecipeCard key={recipe.sys.id} recipe={recipe} />
       ))
       }
+      <style jsx>
+        {`
+          .recipe-list {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            grid-gap: 20px 60px;
+          }
+        `}
+      </style>
     </div>
   );
 }

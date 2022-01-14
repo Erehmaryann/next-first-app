a7bb7d29baea086b24061d868ecacd6631028f5f
erehmaryann <edwardmaryann09@gmail.com>
2022-01-14 13:41:57 -0800

    implemented incremental static regeneration

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export async function getStaticProps() {
   return {
     props: {
       recipes: res.items,
+      revalidate: 1,
     }
   };
 }
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -37,6 +37,7 @@ export async function getStaticProps({ params }) {
   return {
     props: {
       recipe: items[0],
+      revalidate: 1,
     },
   };
 }
